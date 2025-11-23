ba8dce3edc15d0bbf698bbfed1b58d33a927b012
Rohan Puri <purirohan62@gmail.com>
2025-11-23 12:45:46 -0500

    feat: add main.py with FastAPI and CORS middleware

---
+++ server/app/main.py
@@ -0,0 +1,27 @@
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes.upload import router as upload_router
+from app.routes.formats import router as formats_router
+
+app = FastAPI(title="WebMorph")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(formats_router, prefix="/files")
+app.include_router(upload_router, prefix="/files")
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the File Converter API!"}
